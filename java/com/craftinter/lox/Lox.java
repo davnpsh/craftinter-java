542cec9a43a56f57c870a37288baa7171439cf13
davnpsh <me@davnpsh.dev>
2026-03-22 00:55:50 -0500

    add: parser

--- java/com/craftinter/lox/Lox.java
+++ java/com/craftinter/lox/Lox.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+
 	static boolean hadError = false;
 
 	public static void main(String[] args) throws IOException {
@@ -25,7 +26,7 @@ public class Lox {
 	private static void runFile(String path) throws IOException {
 		byte[] bytes = Files.readAllBytes(Paths.get(path));
 		run(new String(bytes, Charset.defaultCharset()));
-		
+
 		// Indicate an error in exit code
 		if (hadError) System.exit(65);
 	}
@@ -47,9 +48,11 @@ public class Lox {
 		Scanner scanner = new Scanner(source);
 		List<Token> tokens = scanner.scanTokens();
 
-		for (Token token : tokens) {
-			System.out.println(token);
-		}
+		Parser parser = new Parser(tokens);
+		Expr expression = parser.parse();
+
+		if (hadError) return;
+		System.out.println(new AstPrinter().print(expression));
 	}
 
 	static void error(int line, String message) {
@@ -59,5 +62,13 @@ public class Lox {
 	private static void report(int line, String where, String message) {
 		System.err.println("[line " + line + "] Error" + where + ": " + message);
 		hadError = true;
+	}
+
+	static void error(Token token, String message) {
+		if (token.type == TokenType.EOF) {
+			report(token.line, " at end", message);
+		} else {
+			report(token.line, " at '" + token.lexeme + "'", message);
+		}
 	}
 }
